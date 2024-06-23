a70bb9745233d1ba5c229a73354d1384b6e39b0a
Masashi SATO <ms2.sato@gmail.com>
2024-06-24 04:47:19 +0900

    create pdf on s3

--- sam-pdf-layer/hello-world/app.ts
+++ sam-pdf-layer/hello-world/app.ts
@@ -1,9 +1,69 @@
 import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda';
-import chromium from '@sparticuz/chromium';
-import puppeteer from 'puppeteer-core';
+import { OpenAndSaveOption, outputPdf } from './pdf';
+import { S3Client, PutObjectCommand, GetObjectCommand, PutObjectCommandInput } from '@aws-sdk/client-s3';
+import { getSignedUrl } from '@aws-sdk/s3-request-presigner';
+import crypto from 'crypto';
+import { RequestPresigningArguments } from '@aws-sdk/types';
 
-chromium.setHeadlessMode = true;
-chromium.setGraphicsMode = false;
+const startTime = performance.now();
+
+if (!process.env.AWS_S3_BUCKET) {
+    throw new Error('AWS_S3_BUCKET is required');
+}
+
+/*
+AWS_S3_BUCKET=pdf-test # simple bucket name
+AWS_S3_BUCKET=pdf-test,pdf-test2 # multiple bucket names
+*/
+
+const buckets = process.env.AWS_S3_BUCKET.split(',');
+console.log(`buckets: ${buckets}`);
+
+let client;
+if (process.env.LOCAL === 'true') {
+    client = new S3Client({
+        credentials: { accessKeyId: 'FAKE', secretAccessKey: 'FAKE' },
+        endpoint: 'http://localstack:4566',
+        forcePathStyle: true,
+    });
+} else {
+    client = new S3Client();
+}
+
+class InputError extends Error {
+    constructor(message: string) {
+        super(message);
+        this.name = 'InputError';
+    }
+}
+
+const checkPayload = (params: ExecuteOptions) => {
+    if (!params.bucket) {
+        throw new InputError('params.bucket is required');
+    }
+    if (buckets.indexOf(params.bucket) === -1) {
+        throw new InputError(`Unexpected params.bucket ${params.bucket}`);
+    }
+    if (!params.key) {
+        throw new InputError('params.key is required');
+    }
+    if (!params.content) {
+        throw new InputError('params.content is required');
+    }
+
+    if (params.option) {
+        if (params.option.signedUrl) {
+            if (typeof params.option.signedUrl !== 'boolean' && typeof params.option.signedUrl !== 'object') {
+                throw new InputError('params.option.signedUrl must be boolean or object');
+            }
+        }
+        if (params.option.pdf) {
+            if (typeof params.option.pdf !== 'object') {
+                throw new InputError('params.option.pdf must be object');
+            }
+        }
+    }
+};
 
 /**
  *
@@ -15,82 +75,91 @@ chromium.setGraphicsMode = false;
  *
  */
 export const lambdaHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
-    let result = null;
-    let browser = null;
-
+    const startTime = performance.now();
     try {
-        const customArgs = [
-            ...chromium.args,
-            '--no-sandbox',
-            '--disable-setuid-sandbox',
-            '--disable-dev-shm-usage',
-            '--disable-gpu',
-            '--no-first-run',
-            '--no-zygote',
-            '--single-process',
-        ];
-        const options = {
-            args: customArgs,
-            defaultViewport: chromium.defaultViewport,
-            executablePath: await chromium.executablePath('/opt/nodejs/node_modules/@sparticuz/chromium/bin'),
-            headless: chromium.headless,
-            ignoreHTTPSErrors: true,
-        };
-
-        console.log('puppeteer.launch(options)');
-        browser = await puppeteer.launch(options);
-
-        console.log('await browser.newPage();');
-        let page = await browser.newPage();
-        const content = `<h1>Hello World</h1>`;
-        await page.setContent(content, {
-            waitUntil: ['load'], // "networkidle0" is too late
-        });
-        const pdf = await page.pdf();
-        console.log('pdf:', pdf);
-
-        const result = pdf.toString('base64');
-
-        // console.log('page.goto');
-        // await page.goto(event.url || 'https://example.com');
-
-        // console.log('page.title()');
-        // result = await page.title();
-        console.log('result:', result);
-
-        return {
-            statusCode: 200,
-            body: result,
-        };
-    } catch (error) {
-        return {
-            statusCode: 500,
-            body: error instanceof Error ? error.message : String(error),
-        };
-    } finally {
-        if (browser !== null) {
-            await browser.close();
+        let payload;
+        if (event.requestContext) {
+            // for function URLs
+            if (event.body === null) {
+                throw new InputError('event.body is required');
+            }
+            payload = JSON.parse(event.body);
+        } else if (event.httpMethod) {
+            // for API Gateway
+            if (event.body === null) {
+                throw new InputError('event.body is required');
+            }
+            payload = JSON.parse(event.body);
+            /*
+        } else if (event.Records && event.Records[0]?.s3) {
+            // for S3
+            throw new Error('Not supported');
+        */
+        } else {
+            // for test
+            payload = event;
         }
+
+        checkPayload(payload);
+
+        const ret = await execute(payload);
+        const response: APIGatewayProxyResult = {
+            statusCode: 200,
+            body: JSON.stringify(ret),
+        };
+
+        console.log(`handler: ${performance.now() - startTime}`);
+        return response;
+    } catch (err) {
+        if (err instanceof InputError) {
+            console.error(`Error(400): ${err.message}`);
+        } else {
+            console.error(`Error(500): ${err instanceof Error ? err.message : String(err)}`);
+        }
+        console.error(err);
+        console.log(`handler(err): ${performance.now() - startTime}`);
+        throw err;
     }
 };
 
-/*
-export const lambdaHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
-    try {
-        return {
-            statusCode: 200,
-            body: JSON.stringify({
-                message: 'hello world',
-            }),
-        };
-    } catch (err) {
-        console.log(err);
-        return {
-            statusCode: 500,
-            body: JSON.stringify({
-                message: 'some error happened',
-            }),
-        };
-    }
+type ExecuteOptions = OpenAndSaveOption & { bucket: string; option?: { signedUrl?: boolean | { expiresIn: number } } };
+
+const execute = async (params: ExecuteOptions) => {
+    const { pdf, key } = await outputPdf(params);
+
+    const ret = await putToS3(pdf, params.bucket, key, params.option?.signedUrl);
+    return { key, ...ret };
 };
-*/
+
+const putToS3 = async (
+    pdf: Buffer,
+    bucket: string,
+    key: string,
+    signedUrlOption?: RequestPresigningArguments | boolean,
+) => {
+    const params: PutObjectCommandInput = {
+        Body: Buffer.from(pdf),
+        Bucket: bucket,
+        ContentType: 'application/pdf',
+        ContentDisposition: 'inline',
+        Key: key ?? `${crypto.randomUUID()}.pdf`,
+    };
+
+    const command = new PutObjectCommand(params);
+    await client.send(command);
+
+    if (signedUrlOption === undefined) {
+        return {};
+    }
+
+    const getObjectCommand = new GetObjectCommand({
+        Bucket: params.Bucket,
+        Key: params.Key,
+    });
+
+    const getSignedUrlOption: RequestPresigningArguments = typeof signedUrlOption === 'boolean' ? {} : signedUrlOption;
+    const signedUrl = await getSignedUrl(client, getObjectCommand, getSignedUrlOption);
+    return { bucket, signedUrl };
+};
+
+console.log(`bootup: ${performance.now() - startTime}`);
---
+++ sam-pdf-layer/hello-world/pdf.ts
@@ -0,0 +1,103 @@
+import path from 'path';
+import fs from 'fs';
+
+import chromium from '@sparticuz/chromium';
+import puppeteer, { Page } from 'puppeteer-core';
+
+chromium.setHeadlessMode = true;
+chromium.setGraphicsMode = false;
+
+// @see https://github.com/puppeteer/puppeteer/blob/main/docs/troubleshooting.md#running-puppeteer-on-gitlabci
+// @see https://qiita.com/masaminh/items/eb9188c15de60b6b1de6#%E3%83%8F%E3%83%9E%E3%81%A3%E3%81%9F%E5%86%85%E5%AE%B9
+
+export const outputPdf = async (params: OpenAndSaveOption) => {
+    const startTime = performance.now();
+    // return await Promise.all(targets.map((target) => openAndSave(target)));
+    const ret = await openAndSave(params);
+    console.log(`outputPdf: ${performance.now() - startTime}`);
+    return ret;
+};
+
+const defaultPdfOption = {
+    format: 'A4',
+    printBackground: true,
+    displayHeaderFooter: true,
+};
+
+let page: Page | null = null;
+
+const getPage = async () => {
+    if (page) {
+        return page;
+    }
+
+    const startTime = performance.now();
+
+    const customArgs = [
+        ...chromium.args,
+        '--no-sandbox',
+        '--disable-setuid-sandbox',
+        '--disable-dev-shm-usage',
+        '--disable-gpu',
+        '--no-first-run',
+        '--no-zygote',
+        '--single-process',
+    ];
+    const options = {
+        args: customArgs,
+        defaultViewport: chromium.defaultViewport,
+        executablePath: await chromium.executablePath('/opt/nodejs/node_modules/@sparticuz/chromium/bin'),
+        headless: chromium.headless,
+        ignoreHTTPSErrors: true,
+    };
+
+    const browser = await puppeteer.launch(options);
+
+    console.log(`openAndSave: puppeteer.launch: ${performance.now() - startTime}`);
+    const startTime1 = performance.now();
+    page = await browser.newPage();
+    console.log(`openAndSave: browser.newPage: ${performance.now() - startTime1}`);
+    return page;
+};
+
+export type OpenAndSaveOption = {
+    key: string;
+    content: string;
+    option?: {
+        pdf?: any;
+    };
+};
+
+const openAndSave = async ({ key, content, option }: OpenAndSaveOption) => {
+    const page = await getPage();
+    const startTime2 = performance.now();
+
+    // @see https://github.com/puppeteer/puppeteer/issues/728
+    // page.on("request", (request) => {
+    //   console.log(`Intercepted request with URL: ${request.url()}`);
+    //   request.continue();
+    // });
+    // await page.goto(`data:text/html,${content}`, {
+    //   waitUntil: 'networkidle0'
+    // });
+
+    await page.setContent(content, {
+        waitUntil: ['load'], // "networkidle0" is too late
+    });
+
+    console.log(`openAndSave: page.setContent: ${performance.now() - startTime2}`);
+    const startTime3 = performance.now();
+
+    const params = { ...defaultPdfOption, ...option?.pdf };
+    let pdf;
+    if (process.env.DEBUG) {
+        const tmpPath = `/tmp/${key}`;
+        fs.mkdirSync(path.dirname(tmpPath), { recursive: true });
+        pdf = await page.pdf({ path: tmpPath, ...params });
+    } else {
+        pdf = await page.pdf(params);
+    }
+    console.log(`openAndSave: page.pdf: ${performance.now() - startTime3}`);
+
+    return { pdf, key };
+};
